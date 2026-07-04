3f438eac9694874f9733052215a2b88977e0866b
Dikshit781 <dikshitdhimanoo4@gmail.com>
2026-07-04 15:40:08 +0530

    Fix CORS Vercel URL

--- backend/app.py
+++ backend/app.py
@@ -10,8 +10,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-        "https://gesture-puzzle-ai-49i3-five.vercel.app"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://gesture-puzzle-ai-4913-five.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
